77910450b025b77617f11e802d93472ac6420292
Pinky Langat <pclangat@users.noreply.github.com>
2017-05-13 09:18:39 +0100

    uploaded scripts

---
+++ barcoded_fluseq_pipeline.sh
@@ -0,0 +1,133 @@
+#! /bin/bash
+# ~/scripts/barcoded-flu-seq/barcoded_fluseq_pipeline.sh -i $sample_prefix
+
+##Paths to software dependencies
+#PEAR: http://sco.h-its.org/exelixis/web/software/pear/
+pear_path='/Users/pclangat/Software/pear/bin/pear'
+quasr_path='/Users/pclangat/scripts/scripts_from_others/QUASR/QUASR_v7.02/readsetProcessor.jar'
+r_path='/usr/local/bin/R'
+barcode_filter_path='/Users/pclangat/scripts/barcoded-flu-seq/primerID_pipeline.py'
+
+##Help message
+if [ "$#" = "0" ]; then
+	echo "[USAGE]: barcoded_fluseq_pipeline.sh <options>
+-h   [None]		Print this help message
+-i * [String]		Input file prefix"
+	exit 1
+fi
+
+##Accepting and checking user input
+while getopts ":i:h" flag; do
+	case $flag in
+		h) echo "[USAGE]: assemblyPipeline_map.sh <options>
+	-h   [None]		Print this help message
+	-i * [String]		Input file prefix"
+	    exit 1
+		;;
+		i) prefix=$OPTARG
+		;;
+		:) echo "[ERROR]: Flag -$OPTARG requires an argument. See -h for details."
+		   exit 1
+		;;
+		?) echo "[INFO]: Ignoring invalid option: -$OPTARG. See -h for details."
+		   exit 1
+	esac
+done
+
+if [ "$prefix" = '' ]; then
+	echo '[ERROR]: Incorrect usage. See -h for details.'
+	exit 1
+fi
+
+##Sample path
+sample_path="${PWD}/$prefix/"
+
+##Create directory with prefix name if it doesn't already exist, move files into it, move into it
+echo "[INFO 1]: Creating directory for sample '$prefix'."
+mkdir -p $sample_path
+
+echo "[INFO 2]: Copying sample files into sample directory."
+for i in `ls ${PWD}/$prefix*`;
+do
+	test -f $i && echo ...$i && cp $i $sample_path/
+done
+
+echo "[INFO 3]: Checking sample directory for read files."
+cd $sample_path
+
+##Get fastq read files
+read1="_R1_001.fastq.gz"
+read2="_R2_001.fastq.gz"
+
+if [ -f $prefix$read1 ]; then
+	echo "... Found read1 '$prefix$read1'"
+	if [ -f $prefix$read2 ]; then
+		echo "... Found read2 '$prefix$read2'"
+	else
+		echo "[ERROR]: Read2 '$prefix$read2' not found."
+	fi
+else
+	echo "[ERROR]: Read1 '$prefix$read1' not found."
+	exit 1
+fi
+
+##Pair fastq files
+echo '[INFO 4]: Pairing read files using PEAR.'
+$pear_path -f $prefix$read1 -r $prefix$read2 -o $prefix
+
+##Check if pairing worked
+if [ -s $prefix.assembled.fastq ]; then
+	echo '... Paired file found OK.'
+else
+	echo "[ERROR]: Paired output file is empty. Problem with pairing occurred."
+	exit 1
+fi 
+
+##Tidy up/zipup fastq files
+echo '[INFO 5]: Cleaning up and compressing fastq files'.
+for fastq in ./$prefix.*.fastq
+do
+	if [ -s $fastq ]; then	
+			gzip -f $fastq
+	else
+		rm $fastq
+	fi
+done
+
+
+##Run QC on paired fastq
+echo '[INFO 6]: Performing quality control using QUASR:'
+java -jar $quasr_path -i $prefix.assembled.fastq.gz -o $prefix -q -l 150 -m 20 -g -R $r_path 
+
+##Check if QC worked
+if [ -s $prefix.qc.fq ]; then
+	echo '... QC reads found.'
+else
+	echo "[ERROR]: QC output file is empty/not found. Problem with QC occurred."
+	exit 1
+fi
+
+##Barcode filtering
+echo '[INFO 7]: Filtering reads with sequencing barcodes.'
+python $barcode_filter_path $prefix
+
+##Finish
+echo '[INFO 8]: Cleaning up and compressing remaining fastq files'.
+for fastq in ./*.fq
+do
+	if [ -s $fastq ]; then	
+			gzip -f $fastq
+	else
+		rm $fastq
+	fi
+done
+
+mkdir ./intermediate_fastqs
+for gz in ./*.gz
+do
+	mv $gz ./intermediate_fastqs
+done
+
+cd ..
+echo "Done."
+mv log.$prefix.txt $sample_path
--- primerID_pipeline.py
+++ primerID_pipeline.py
@@ -1,58 +1,61 @@
 #!/usr/bin/python
 ## ~/scripts/primerID_pipeline.py <sample_name>
 
-## Takes as input the sample name --> finds fwd and reverse reads in fastq.gz file
-## STEP ONE: Pairs forward and reverse. Requires PEAR.
-## STEP TWO: Does QC via QUASR. Requires QUASR.
+## Takes as input the sample name
+## STEP ONE: 
+
+'''Requirements: 
+requires pip: brew install pip
+requires biopython: pip install biopython
+requires regex: pip install regex'''
 
 ##Import modules
-import sys, os, regex
+import sys, os, regex, subprocess, operator
 from collections import defaultdict
-from Bio import SeqIO
+from Bio import SeqIO, AlignIO
 from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.Align.Applications import MuscleCommandline
+from StringIO import StringIO
+from Bio.Align import AlignInfo
 
 ###METHODS
+def get_sample_reads(prfx):
+	qc_paired_fastq = prfx+'.qc.fq'
+	qc_paired_fasta = prfx+'.qc.fas'
+	
+	SeqIO.convert(qc_paired_fastq, "fastq", qc_paired_fasta, "fasta")
+	reads = SeqIO.index(qc_paired_fasta, 'fasta')
+	return reads
 
-##Ensure correct directory created and all files moved
-#def ensure_correct_path(p):
-#	current_path = os.getcwd()
-#	sample_path = ("%s/%s" % (current_path, p))
-#	
-#	##Create sample directory
-#	if not os.path.exists(sample_dir):
-#		os.makedirs(sample_dir)
-#		print("Creating directory for sample %s" % p)
-#	
-#	##Move files to directory
-
-def get_sequence_file(prfx, sufx, type):
-	if 'fastq' in type:
-		ext = '.fastq.gz'
-	elif 'fasta' in type:
-		ext = '.fas'
-	else:
-		print("Error finding sequence file type: %s" % type)
-		sys.exit(1)
-	file_name = prfx + sufx + ext
-	return file_name
-
-def run_PEAR_pairing(prfx, f1, f2):
-	print("Attempting to pair %s and %s" % (f1, f2))
-	pair_command = pear_path + ' -f ' + f1 + ' -r ' + f2 + ' -o ' + prfx
-	os.system(pair_command)
-	return
-
-def run_QUASR_qc(fastq, prfx, length, phred):
-	print("Attempting to run QC on " + fastq)
-	qc_command = 'java -jar ' + quasr_path+'/QUASR_v7.02/readsetProcessor.jar -i ' + fastq + ' -o ' + prfx + ' -q -l ' + str(length) + ' -m ' + str(phred) + ' -g -R ' + r_path
-	os.system(qc_command)
-	return
-
+def check_barcodes(reads_dict):
+	matches = []
+	intact_barcoded_seqs = 0
+	barcoded_seqs = defaultdict(list)
+	
+	##For each read, look for reverse primer (either full or partial match)
+	for read in reads_dict:
+		sequence = reads_dict[read].seq
+		primer_pos, oriented_sequence, match_depth = check_rev_primer_match(rev_primer, sequence)
+		matches.append(match_depth)
+		
+		##If rev primer sequence is found, check if intact rev barcode pattern exists
+		if primer_pos > 0:
+			rev_primer_seq = oriented_sequence[primer_pos:]
+			intact_rev_barcode = regex.findall(pattern, str(rev_primer_seq))
+			
+			##If intact rev barcode pattern, add barcode and associated sequences to barcodes dict
+			if intact_rev_barcode:
+				intact_barcoded_seqs += 1
+				barcode = (intact_rev_barcode[0]).split(rev_primer)[1]
+				barcoded_seqs[barcode].append(oriented_sequence)
+	return matches, intact_barcoded_seqs, barcoded_seqs
+	
 def check_rev_primer_match(r_primer, seq):
 	match = 'none'
 	pos = seq.find(r_primer)
 	
-	## if match found
+	##if match found
 	if pos > -1:
 		match = 'full'
 	##if no match found
@@ -65,17 +68,48 @@ def check_rev_primer_match(r_primer, seq):
 		if pos > -1:
 			seq = rc_seq
 			match = 'full'
-		#else:
+		else:
 			##if still no match, try searching for partial match
-				
+			pos, seq, match = check_partial_rev_primer_match(r_primer, seq)
+	return pos, seq, match
+	
+def check_partial_rev_primer_match(r_primer, seq):
+	pos = -1
+	match = 'none'
+	
+	pattern = '('+r_primer+'){e<=2}'
+	fuzzy_match = regex.search(pattern, str(seq), regex.BESTMATCH)
+	
+	##if partial match found
+	if fuzzy_match:
+		match = 'partial'
+		pos = fuzzy_match.span()[1]
+	##if no partial match found
+	else:
+		##try reverse complement of sequence
+		rc_seq = seq.reverse_complement()
+		fuzz_match = regex.search(pattern, str(rc_seq), regex.BESTMATCH)
+		
+		##if partial match, re-orient sequence:
+		if fuzzy_match:
+			seq = rc_seq
+			match = 'partial'
+			pos = fuzzy_match.span()[1]
 	return pos, seq, match
 		
-def fuzzy_substring(needle, haystack):
+def fuzzy_substring(n, h):
 	"""Calculates the fuzzy match of needle in haystack,
 	using a modified version of the Levenshtein distance
 	algorithm.
 	The function is modified from the levenshtein function
 	in the bktree module by Adam Hupp"""
+	if len(n) > len(h):
+		needle = h
+		haystack = n
+	else:
+		needle = n
+		haystack = h
+		
 	m, n = len(needle), len(haystack)
     # base cases
 	if m == 1:
@@ -92,97 +126,202 @@ def fuzzy_substring(needle, haystack):
                                row1[j]+cost) #substitution
                            )
 		row1 = row2
-		print row1
-	print min(row1)
-	print (0.3*m)
-	if min(row1) < (0.3*m):
-		primer_pos = row1.index(min(row1)) - m
-	else:
-		primer_pos = -1
-	return primer_pos
+		#print row1
+	#print min(row1)
+	#print (0.3*m)
+	#if min(row1) < (0.3*m):
+	#	primer_pos = row1.index(min(row1)) - m
+	#else:
+	#	primer_pos = -1
+	#return primer_pos
+	return min(row1)
+	
+##Generate alignment for all seqs with same barcode
+def get_alignment(records):
+	##open up muscle
+	child = subprocess.Popen(str(muscle_cline),
+							stdin=subprocess.PIPE,
+							stdout=subprocess.PIPE,
+							stderr=subprocess.PIPE,
+							universal_newlines=True,
+							shell=(sys.platform!="win32"))
+	SeqIO.write(records, child.stdin, "fasta")
+	child.stdin.close()
+	align = AlignIO.read(child.stdout, "fasta")
+	return align
+
+def generate_consensus(alignment):
+	align_summary = AlignInfo.SummaryInfo(alignment)
+	consensus = align_summary.dumb_consensus(threshold=0.51, ambiguous='N')
+	return consensus
 				
 ###INITIALISATIONS
 ##Paths to software
-quasr_path = "/Users/pl6/scripts/scripts_from_others/QUASR/"
-r_path = "/usr/bin/R"
-pear_path = "/Users/pl6/Software_downloaded/pear/bin/pear"
-
-f_sufx = "_R1_001"
-r_sufx = "_R2_001"
+muscle_path = '/Users/pclangat/Software/muscle/muscle3.8.31_i86darwin32'
 
 ##These will become user-accepted later
 min_length = 125
 min_phred = 20
+min_barcode_count = 2
 
 fwd_primer = "CGGGGAAAATATGCAACAATCCT"
 rev_primer = "GAGGGTTTCACTTGGACTGGG"
 full_rev_primer = "GAGGGTTTCACTTGGACTGGGNNNNANNNNANNNNAAAGCAGTGGTATCAACGCA"
 pattern = rev_primer+'[A-Z]{4}A[A-Z]{4}A[A-Z]{4}A'
-pattern = regex.compile(pattern)
+#pattern = regex.compile(pattern)
+pattern = regex.compile('('+pattern+'){e<=5}')
 
+muscle_cline = MuscleCommandline(muscle_path)
+
+reference_file = '/Users/pclangat/Desktop/Projects/2-PrimerID_pipeline/1-daniel_MiSeq_2017-03-01/reference.txt'
+reference_seq = ''
+with open(reference_file, 'r') as fh:
+	for line in fh:
+		reference_seq += line.strip()
+#print reference_seq
+		
 ###MAIN
 if __name__ == '__main__':
-	##Parse user input
+	##STEP 1: Parse user input
 	try:
-		prfx = sys.argv[1]
+		prefix = sys.argv[1]
 	except:
 		print('[USAGE]: %s sample_prefix' % sys.argv[0] )
 		sys.exit(1)
 
-	##Get fastq files
-	#fastq1 = get_sequence_file(prfx, f_sufx, 'fastq')
-	#fastq2 = get_sequence_file(prfx, r_sufx, 'fastq')
+	###STEP 2: Convert qc fastq files to fasta file and load records as dictionary index (i.e. does not save all into memory, good for large fastq files)
+	reads_dict = get_sample_reads(prefix)
+	print "\n>>>BARCODE FILTERING & MOLECULAR COUNTING SUMMARY<<<"		
+	print "[INFO 1]: Total input sequences (after pairing and QC): %s" % len(reads_dict)
 	
-	##Pair the fastq files
-	#run_PEAR_pairing(prfx, fastq1, fastq2)
-	#paired_fastq = prfx + '.assembled.fastq'
+	###STEP 3: Find reverse primer (gene portion) in read sequence, count whether full or partial or no match
+	matches, intact_barcoded_seqs, barcoded_seqs = check_barcodes(reads_dict)
+	reads_dict.close()
+	m = len(matches) - matches.count('none')
+	percent_m = (m*100.0/len(matches))
+	percent_i = (intact_barcoded_seqs*100.0/m)
+	percent_b = (len(barcoded_seqs)*100.0/intact_barcoded_seqs)
 	
-	##Run QC on paired fastq
-	#run_QUASR_qc(paired_fastq, prfx, min_length, min_phred)
-	qc_paired_fastq = prfx + '.qc.fq'
-	qc_paired_fasta = prfx + '.fas'
-	
-	##Convert qc fastq files to fasta files
-	#SeqIO.convert(qc_paired_fastq, "fastq", qc_paired_fasta, "fasta")
+	print "[INFO 2]: Sequences with reverse primer region: %s (%.1f%%)" % (m, percent_m)
+	print "... Full matches to reverse primer: %s" % matches.count('full')
+	print "... Partial matches to reverse primer: %s" % matches.count('partial')
+	print "... Reverse primer not found: %s" % matches.count('none')
+	print "[INFO 3]: Sequences with intact barcodes: %s (%.1f%%)" % (intact_barcoded_seqs, percent_i)
+	print "... Representing # of unique barcodes: %s (%.1f%%)" % (len(barcoded_seqs), percent_b)
 
-	##Load qc FASTAs as dicts. SeqIO only reads up to the space, so now the names should match in 1 and 2
-	read_records = SeqIO.index(qc_paired_fasta, 'fasta')
-	reads_dict = {}
+	###STEP 4: Count number of seqs associated to each barcode (i.e. barcode multiplicity/count), decide whether to process
+	print "[INFO 4]: Checking barcode multiplicity and filtering"
+	barcodes_to_process_count = 0
+	barcode_distribution = {}
+	consensus_records = []
 	
-	##Only keeps what is needed from SeqIO record in dict
-	for read in read_records:
-		reads_dict[read] = read_records[read].seq
-	read_records.close()
+	##For each barcode
+	b=1
+	for barcode in barcoded_seqs:
+		print "barcode %s" % b
+		barcode_count = len(barcoded_seqs[barcode])	
+		
+		##Add frequency of barcode counts to a dictionary for count distribution
+		if barcode_count in barcode_distribution:
+			barcode_distribution[barcode_count] += 1
+		else:
+			barcode_distribution[barcode_count] = 1
+			
+		##If barcode multiplicity is greater than minimum (simple case: 2)
+		##add to list of records of barcoded seqs to process
+		if barcode_count > min_barcode_count:
+			barcodes_to_process_count += 1
+			i = 1
+			barcode_group_records = []
+			
+			##for each sequence in that barcode group, name it and create seq record
+			for sequence in barcoded_seqs[barcode]:
+				id_name = '%s-%s-%s' % (barcode, barcode_count, i)
+				i += 1
+				record = SeqRecord(sequence, id=id_name, description='')
+				barcode_group_records.append(record)
+			
+			##Take barcode_group and align it
+			print "getting alignment"
+			aligned_barcode_group = get_alignment(barcode_group_records)
+			
+			##Get consensus of alignment
+			consensus_name = '%s-%s-%s' % (prefix, barcode, barcode_count)
+			consensus_seq = generate_consensus(aligned_barcode_group)
+
+			if 'N' not in consensus_seq:
+				consensus_rec = SeqRecord(consensus_seq, id=consensus_name, description='')
+				consensus_records.append(consensus_rec)	
+		b+=1
+		
+	print "... Barcode multiplicity (read count) distribution:"
+	for i in barcode_distribution:
+		print "\t%s\t%s" % (i, barcode_distribution[i])
 	
-	##Find reverse primer (gene portion) in read sequence, count whether full or partial or no match
-	matches = []
-	barcodes=0
-	barcoded_seqs = defaultdict(dict)
-	for read in reads_dict:
-		sequence = reads_dict[read]
-		primer_pos, oriented_sequence, match_depth = check_rev_primer_match(rev_primer, sequence)
-		matches.append(match_depth)
-		reads_dict[read] = oriented_sequence
-		if primer_pos > 0:
-			id = oriented_sequence[primer_pos:]
-			id = regex.findall(pattern, str(id))
-			if id:	
-				barcodes+=1
-				barcode = id[0].split(rev_primer)[1]
-				if oriented_sequence in barcoded_seqs[barcode]:
-					barcoded_seqs[barcode][oriented_sequence] += 1 
-				else:
-					barcoded_seqs[barcode][oriented_sequence] = 1 
-	print barcoded_seqs
-	print "Full matches to reverse primer: %s" % matches.count('full')
-	print "Partial matches to reverse primer: %s" % matches.count('partial')
-	print "Reverse primer not found: %s" % matches.count('none')
-	print "Intact barcodes found: %s" % barcodes
+	percent_p = 100.0*barcodes_to_process_count/len(barcoded_seqs)
+	percent_c = 100.0*len(consensus_records)/barcodes_to_process_count
 	
-	##Barcode filtering
+	print "... Barcodes with multiplicity >%s to process: %s (%.1f%%)" % (min_barcode_count, barcodes_to_process_count, percent_p)
+	print "... Total unambiguous consensus (i.e. template) sequences: %s (%.1f%%)" % (len(consensus_records), percent_c)
+	print "... Output generated to file %s.barcode_filtered.fas with headers '>Sample_Name-Barcode-#Reads'" % prefix
+	##Put barcodes to process into a fasta file
+	SeqIO.write(consensus_records, '%s.barcode_filtered.fas' % prefix, 'fasta')
+	
+	###STEP 5: Compare template sequences and group matching ones together and count #templates
+	print "[INFO 5]: Checking template sequences for unique sequence variants"
+	template_sequences = []
+	unique_sequences = {}
+	
+	##Creates ordered list of pre-barcode sequences, sorted by sequence length
+	for record in consensus_records:
+		seq = record.seq
+		seq = str(seq[:-34])
+		template_sequences.append(seq)
+	template_sequences.sort(key = len, reverse=True)
+	
+	##Check each template sequence for unique sequences
+	for seq in template_sequences:
+		is_substring = False
+		
+		if seq in unique_sequences:
+			unique_sequences[seq] += 1
+		else:
+			for longer_seq in unique_sequences:
+				##check if it is substring of sequence already in unique_sequences
+				if seq in longer_seq:
+					unique_sequences[longer_seq] += 1
+					is_substring = True
+					"substring found!"
+					break
+			##if still unique (i.e. not a substring), add to dictionary of counts
+			if not is_substring:	
+				unique_sequences[seq] = 1
+	
+	confident_unique_seqs = list(k for k, v in unique_sequences.items() if v > 1)
+	
+	print "... Total unique sequence variants: %s" % len(unique_sequences)
+	print "... Unique sequence variants associated with >1 template molecules: %s" % len(confident_unique_seqs) 
+	
+	###STEP 6: Name and align sequences, generate fasta output
+	print "[INFO 6]: Creating output fasta '%s.unique_sequences.fas' of aligned unique sequence variants with headers '>Sample_Name-#AssociatedTemplates'" % prefix
+	##Header is sample prefix+number of templates associated with sequence
+	unique_records = []
+	for s in unique_sequences:
+		sequence = Seq(s)
+		name = '%s-%s'  % (prefix, unique_sequences[s])
+		seq_rec = SeqRecord(sequence, id=name, description='')
+		unique_records.append(seq_rec)
+	
+	##Add reference sequence 
+	reference_rec = SeqRecord(Seq(reference_seq), id='reference-sequence', description='')
+	unique_records.append(reference_rec)
+	
+	##Align the unique sequences and order with most templates at top
+	aligned_unique_records = get_alignment(unique_records)
+	aligned_unique_records.sort(reverse=True)
+	
+	##Write to fasta file
+	SeqIO.write(aligned_unique_records, '%s.unique_sequences.fas' % prefix, 'fasta')
 	
 	##Wrap up
-	print "Done."	
-		
-		
-		
+	#print "Done."	
